a0b35abd4509a8e349d8f4a4b42d3aa8d0f0e444
Peter J <117600297+peterisOnIT@users.noreply.github.com>
2024-05-04 00:05:01 -0700

    Update auth_overflow2_fixed.c

--- auth_overflow2_fixed.c
+++ auth_overflow2_fixed.c
@@ -5,28 +5,30 @@
 // Random function goes here
 
 int check_authentication(char *password) {
- char password_buffer[16];
+    char password_buffer[16];
 
- strcpy(password_buffer, password);
+    // Use strncpy to avoid buffer overflow
+    strncpy(password_buffer, password, sizeof(password_buffer) - 1);
+    password_buffer[sizeof(password_buffer) - 1] = '\0'; // Ensure null termination
 
- if(strcmp(password_buffer, "michael") == 0)
- return 1;
- else if(strcmp(password_buffer, "tsikerdekis") == 0)
- return 1;
- else
- return 0;
+    if(strcmp(password_buffer, "michael") == 0)
+        return 1;
+    else if(strcmp(password_buffer, "tsikerdekis") == 0)
+        return 1;
+    else
+        return 0;
 }
 
 int main(int argc, char *argv[]) {
- if(argc < 2) {
- printf("Usage: %s <password>\n", argv[0]);
- exit(0);
- }
- if(check_authentication(argv[1])) {
- printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
- printf(" Access Granted.\n");
- printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
- } else {
- printf("\nAccess Denied.\n");
- }
+    if(argc < 2) {
+        printf("Usage: %s <password>\n", argv[0]);
+        exit(0);
+    }
+    if(check_authentication(argv[1])) {
+        printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+        printf(" Access Granted.\n");
+        printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    } else {
+        printf("\nAccess Denied.\n");
+    }
 }
